5aea64d69ce5067f243881bac6a75af0f6ad858a
Julio Zittei <julio.zittei96@gmail.com>
2022-04-27 03:45:30 -0300

    Refactor Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,8 +4,8 @@ import { ReactNode } from 'react';
 interface LinkProps {
   children: ReactNode;
   href: string;
-  target: string;
-  rel: string;
+  target?: string;
+  rel?: string;
 }
 
 export function Link({ href, target, rel, children, ...props }: LinkProps): JSX.Element {
