0acfe340edea5632fc8a93dce22fe4ec6c35f3d7
Julio Zittei <julio.zittei96@gmail.com>
2022-04-27 03:40:26 -0300

    Refactor stiches tokens

--- src/styles/stiches.config.ts
+++ src/styles/stiches.config.ts
@@ -1,76 +1,98 @@
 import { createStitches } from '@stitches/react';
 import {
-  skyA,
-  skyDarkA,
-  pinkA,
-  pinkDarkA,
-  redA,
-  redDarkA,
-  yellowA,
-  yellowDarkA,
-  greenA,
-  greenDarkA,
-  slateA,
-  slateDarkA,
-  tealA,
-  blueA,
-  tealDarkA,
-  blueDarkA,
+  sky,
+  skyDark,
+  pink,
+  pinkDark,
+  red,
+  redDark,
+  yellow,
+  yellowDark,
+  green,
+  greenDark,
+  slate,
+  slateDark,
+  teal,
+  tealDark,
+  blue,
+  blueDark,
+  amber,
+  amberDark,
+  blackA,
+  whiteA,
 } from '@radix-ui/colors';
 
 const { css, globalCss, keyframes, theme, createTheme, getCssText, config, styled } = createStitches({
   theme: {
     colors: {
-      ...pinkA,
-      ...skyA,
-      ...greenA,
-      ...slateA,
-      ...redA,
-      ...yellowA,
-      ...tealA,
-      ...blueA,
-      background: '#fff',
+      ...pink,
+      ...sky,
+      ...green,
+      ...slate,
+      ...red,
+      ...yellow,
+      ...teal,
+      ...blue,
+      ...amber,
+      ...blackA,
+      ...whiteA,
+      backgroundDefault: slate.slate1,
     },
     space: {
-      spaceXS: '0.25rem', // 4px
-      spaceSM: '0.5rem', // 8px
-      spaceDefault: '1rem', // 16px
-      spaceMD: '1.5rem', // 24px
-      spaceLG: '2rem', // 32px
-      spaceXL: '4rem', // 64px
-    },
-    fontSizes: {
-      fontSize2XS: '0.625rem', // 10px
-      fontSizeXS: '0.75rem', // 12px
-      fontSizeSM: '0.875rem', // 14px
-      fontSizeDefault: '1rem', // 16px
-      fontSizeMD: '1.125rem', // 18px
-      fontSizeLG: '1.5rem', // 24px
-      fontSizeXL: '2.25rem', // 36px
-      fontSize2XL: '2.5rem', // 40px
-      fontSize3XL: '3rem', // 48px
-      fontSize4XL: '3.75rem', // 60px
-      fontSize5XL: '4.75rem', // 76px
-      fontSize6XL: '6rem', // 96px
+      space4: '0.25rem', // 4px
+      space8: '0.5rem', // 8px
+      space12: '0.75rem', // 12px
+      space16: '1rem', // 16px
+      space24: '1.5rem', // 24px
+      space32: '2rem', // 32px
+      space64: '4rem', // 64px
     },
     fonts: {
       cinzelFont: "'Cinzel Decorative', cursive",
       poppinsFont: "'Poppins', sans-serif",
     },
-    fontWeights: {},
+    fontSizes: {
+      fontSize10: '0.625rem', // 10px
+      fontSize12: '0.75rem', // 12px
+      fontSize14: '0.875rem', // 14px
+      fontSize15: '0.9375rem', // 15px
+      fontSize16: '1rem', // 16px
+      fontSize18: '1.125rem', // 18px
+      fontSize20: '1.25rem', // 20px
+      fontSize24: '1.5rem', // 24px
+      fontSize36: '2.25rem', // 36px
+      fontSize40: '2.5rem', // 40px
+      fontSize48: '3rem', // 48px
+      fontSize60: '3.75rem', // 60px
+      fontSize76: '4.75rem', // 76px
+      fontSize96: '6rem', // 96px
+    },
+    fontWeights: {
+      fontWeightLight: 300,
+      fontWeightNormal: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
+    },
     lineHeights: {
-      lineHeight2XS: '0.875rem', // 14px
-      lineHeightXS: '1rem', // 16px
-      lineHeightSM: '1.125rem', // 18px
-      lineHeightDefault: '1.25rem', // 20px
-      lineHeightMD: '1.375rem', // 22px
-      lineHeightLG: '1.75rem', // 28px
-      lineHeightXL: '2.5rem', // 40px
-      lineHeight2XL: '2.75rem', // 44px
-      lineHeight3XL: '3.25rem', // 52px
-      lineHeight4XL: '4rem', // 64px
-      lineHeight5XL: '5rem', // 80px
-      lineHeight6XL: '6.25rem', // 100px
+      lineHeight14: '0.875rem', // 14px
+      lineHeight16: '1rem', // 16px
+      lineHeight18: '1.125rem', // 18px
+      lineHeight19: '1.1875rem', // 19px
+      lineHeight20: '1.25rem', // 20px
+      lineHeight22: '1.375rem', // 22px
+      lineHeight24: '1.5rem', // 24px
+      lineHeight28: '1.75rem', // 28px
+      lineHeight40: '2.5rem', // 40px
+      lineHeight44: '2.75rem', // 44px
+      lineHeight52: '3.25rem', // 52px
+      lineHeight64: '4rem', // 64px
+      lineHeight80: '5rem', // 80px
+      lineHeight100: '6.25rem', // 100px
+    },
+    media: {
+      sm: '(max-width: 768px)',
+      md: '(max-width: 992px)',
+      lg: '(min-width: 993px)',
     },
     letterSpacings: {},
     sizes: {},
@@ -81,40 +103,37 @@ const { css, globalCss, keyframes, theme, createTheme, getCssText, config, style
     zIndices: {},
     transitions: {},
   },
-  media: {
-    sm: '(min-width: 0px)',
-    md: '(min-width: 768px)',
-    lg: '(min-width: 1024px)',
+  utils: {
+    pdx: (value: unknown) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    pdy: (value: unknown) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
   },
 });
 
 const darkTheme = createTheme({
   colors: {
-    ...pinkDarkA,
-    ...skyDarkA,
-    ...greenDarkA,
-    ...slateDarkA,
-    ...redDarkA,
-    ...yellowDarkA,
-    ...greenDarkA,
-    ...tealDarkA,
-    ...blueDarkA,
-    background: '#121212',
+    ...pinkDark,
+    ...skyDark,
+    ...greenDark,
+    ...slateDark,
+    ...redDark,
+    ...yellowDark,
+    ...greenDark,
+    ...tealDark,
+    ...blueDark,
+    ...amberDark,
+    backgroundDefault: slateDark.slate1,
   },
-  space: {},
-  fontSizes: {},
-  fonts: {},
-  fontWeights: {},
-  lineHeights: {},
-  letterSpacings: {},
-  sizes: {},
-  borderWidths: {},
-  borderStyles: {},
-  radii: {},
-  shadows: {},
-  zIndices: {},
-  transitions: {},
 });
+
+type Color = {
+  [key: string]: object | undefined;
+};
 
 const globalStyles = globalCss({
   '*': {
@@ -123,30 +142,9 @@ const globalStyles = globalCss({
     boxSizing: 'border-box',
   },
 
-  h1: {
-    fontSize: '$fontSizeXL',
-    lineHeight: '$lineHeightXL',
-  },
-  h2: {
-    fontSize: '$fontSizeLG',
-    lineHeight: '$lineHeightLG',
-  },
-  h3: {
-    fontSize: '$fontSizeMD',
-    lineHeight: '$lineHeightMD',
-  },
-  h4: {
-    fontSize: '$fontSizeDefault',
-    lineHeight: '$lineHeighDefault',
-  },
-  h5: {
-    fontSize: '$fontSizeSM',
-    lineHeight: '$lineHeightSM',
-  },
-  h6: {
-    fontSize: '$fontSizeXS',
-    lineHeight: '$lineHeightXS',
+  body: {
+    backgroundColor: '$backgroundDefault',
   },
 });
 
-export { styled, css, globalCss, keyframes, theme, darkTheme, createTheme, getCssText, config, globalStyles };
+export { styled, css, globalCss, globalStyles, keyframes, theme, darkTheme, createTheme, getCssText, config };
