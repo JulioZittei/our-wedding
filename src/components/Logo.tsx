72725c07ee1461ab50362e4d423a4170dff86a47
Julio Zittei <julio.zittei96@gmail.com>
2022-05-13 14:14:17 -0300

    Add animation to Header component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,24 +1,55 @@
 import { styled } from '../styles/stiches.config';
 import * as LogoCss from '../styles/components/LogoCss';
 import { Link } from './Link';
+import { motion } from 'framer-motion';
 
 const Root = styled(Link, LogoCss.Root);
-const ContentWrapper = styled('div', LogoCss.ContentWrapper);
+const ContentWrapper = styled(motion.div, LogoCss.ContentWrapper);
 const Title = styled('span', LogoCss.Title);
-const Separator = styled('span', LogoCss.Separator);
+const Separator = styled(motion.span, LogoCss.Separator);
 const SubTitle = styled('span', LogoCss.SubTitle);
 
 export function Logo() {
+  const defaultAnimationVariant = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1 },
+  };
   return (
     <Root href="/">
-      <ContentWrapper>
+      <ContentWrapper
+        layout
+        initial={`hidden`}
+        animate={`visible`}
+        variants={defaultAnimationVariant}
+        transition={{
+          duration: 0.6,
+        }}
+      >
         <Title>Élen</Title>
         <SubTitle align="right">Sousa</SubTitle>
       </ContentWrapper>
 
-      <Separator>&</Separator>
+      <Separator
+        layout
+        initial={`hidden`}
+        animate={`visible`}
+        variants={defaultAnimationVariant}
+        transition={{
+          duration: 0.6,
+        }}
+      >
+        &
+      </Separator>
 
-      <ContentWrapper>
+      <ContentWrapper
+        layout
+        initial={`hidden`}
+        animate={`visible`}
+        variants={defaultAnimationVariant}
+        transition={{
+          duration: 0.6,
+        }}
+      >
         <Title>Julio</Title>
         <SubTitle align="left">Zittei</SubTitle>
       </ContentWrapper>
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -9,13 +9,14 @@ import NextLink from 'next/link';
 import useMatchMedia from '../hooks/useMatchMedia';
 import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
 import * as NavMenuCss from '../styles/components/NavigationMenuCss';
+import { motion } from 'framer-motion';
 
 const Root = styled(NavigationMenuPrimitive.Root, NavMenuCss.Root);
 const Container = styled('div', NavMenuCss.Container);
 const List = styled(NavigationMenuPrimitive.List, NavMenuCss.List);
 const Item = styled(NavigationMenuPrimitive.Item, NavMenuCss.Item);
 const Link = styled(NavigationMenuPrimitive.Link, NavMenuCss.Link);
-const Button = styled('button', NavMenuCss.Button);
+const Button = styled(motion.button, NavMenuCss.Button);
 
 type NavigationProps = React.ComponentProps<typeof NavigationMenuPrimitive.Root> & {};
 
@@ -24,6 +25,10 @@ export function NavigationMenu({}: NavigationProps): JSX.Element {
   const { setTheme, resolvedTheme } = useTheme();
 
   const matchedWidthMobile = useMatchMedia('max-width: 768px');
+  const defaultAnimationVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
 
   useEffect(() => setMounted(true), []);
 
@@ -58,7 +63,17 @@ export function NavigationMenu({}: NavigationProps): JSX.Element {
             })}
 
           <Item>
-            <Button onClick={handleToggleTheme} aria-label="Alternar entre o tema light e dark">
+            <Button
+              onClick={handleToggleTheme}
+              aria-label="Alternar entre o tema light e dark"
+              layout
+              initial={`hidden`}
+              animate={`visible`}
+              variants={defaultAnimationVariant}
+              transition={{
+                duration: 0.6,
+              }}
+            >
               <HiOutlineSun />
             </Button>
           </Item>
