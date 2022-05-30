0cf6ccbdfcdc7ce5fe805930c5d20e68f12862a1
Julio Zittei <julio.zittei96@gmail.com>
2022-05-30 15:24:29 -0300

    Add dependencies

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+  ],
+
+  callbacks: {
+    async session({ session }) {
+      return {
+        ...session,
+      };
+    },
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+});
